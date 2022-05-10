ff3f9ea5d235846578a3d381f77edbc4daf73d2f
{tayoshiieee} <{peanutspotatopudding.gmail.com}>
2022-05-10 23:06:10 +0900

    ダウンロードボタンの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import Link from 'next/link';
+
 const Home: React.FC = () => (
-  <main className='mx-auto max-w-6xl py-6'>
-    <h1 className='text-lg font-bold text-primary-800'>
-      Welcome to Kikagaku Next.js Starter Kit!!
-    </h1>
-  </main>
+  <>
+    <header>
+      <button className='rounded bg-primary-500 py-2 px-4 font-bold text-white hover:bg-primary-700'>
+        <Link href='https://www.netflix.com/'>
+          <a> 資料ダウンロード</a>
+        </Link>
+      </button>
+    </header>
+    <main className='mx-auto max-w-6xl py-6'>
+      <h1 className='text-lg font-bold text-primary-800'>Welcome to Kikagaku Next.js Starter!!</h1>
+    </main>
+  </>
 );
 
 export default Home;
