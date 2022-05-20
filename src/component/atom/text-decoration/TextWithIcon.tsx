e75ebf770f8f082dd54af9101583dc24db87e979
{tayoshiieee} <{peanutspotatopudding.gmail.com}>
2022-05-20 20:14:16 +0900

    チェックマーク修正

--- src/component/atom/text-decoration/TextWithIcon.tsx
+++ src/component/atom/text-decoration/TextWithIcon.tsx
@@ -12,7 +12,7 @@ export const TextWithIcon: React.FC<Props> = (props: Props) => {
       <div className={isUnCheck ? 'opacity-30' : 'opacity-100'}>
         <div className='flex items-center'>
           <Image
-            src={isUnCheck ? '/img/check-off.png' : '/img/check.png'}
+            src={isUnCheck ? '/img/check-off.png' : '/img/check-on.png'}
             alt='check-mark'
             width={36}
             height={36}
