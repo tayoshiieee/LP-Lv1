20d098fc6e0f0b4d4802c4dc85323eec131cf183
{tayoshiieee} <{peanutspotatopudding.gmail.com}>
2022-05-21 15:09:45 +0900

    画像ファイル名変更

--- src/component/atom/text-decoration/TextWithIcon.tsx
+++ src/component/atom/text-decoration/TextWithIcon.tsx
@@ -12,7 +12,7 @@ export const TextWithIcon: React.FC<Props> = (props: Props) => {
       <div className={isUnCheck ? 'opacity-30' : 'opacity-100'}>
         <div className='flex items-center'>
           <Image
-            src={isUnCheck ? '/img/check-off.png' : '/img/check-on.png'}
+            src={isUnCheck ? '/img/unCheck.png' : '/img/check.png'}
             alt='check-mark'
             width={36}
             height={36}
